19b21a8dc6118592806609825e8126d428e8a599
Joey Robinson <theamazingness@gmail.com>
2020-11-01 15:24:21 -0500

    work

---
+++ src/components/experience/experience.interapt.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const InteraptExperience = () => (
+  <div className="experience--interapt">
+    <h3>Interapt Experience</h3>
+    <ul>
+      <li>
+        Training with Erica Judd:
+        <p>
+          Over the past 2 weeks, and some time to come, I have trained along
+          side Erica to prepare new hires for their time on the phone.
+        </p>
+      </li>
+      <li>two</li>
+      <li>three</li>
+    </ul>
+  </div>
+)
+
+export default InteraptExperience
--- src/components/experience/experience.main.js
+++ src/components/experience/experience.main.js
@@ -1,9 +1,11 @@
 import React from "react"
+import InteraptExperience from "./experience.interapt"
 
 const Experience = () => (
   <>
     <section className="experience">
       <h2>Experience</h2>
+      <InteraptExperience />
     </section>
   </>
 )
